b04203fadf528ecc97093653f7459031761b6c20
starLLong <2554868271@qq.com>
2022-04-16 16:14:42 +0800

    Initial commit

---
+++ entry/src/main/java/com/starlong/miduapp/MyApplication.java
@@ -0,0 +1,10 @@
+package com.starlong.miduapp;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/ohosTest/java/com/starlong/miduapp/ExampleOhosTest.java
@@ -0,0 +1,14 @@
+package com.starlong.miduapp;
+
+import ohos.aafwk.ability.delegation.AbilityDelegatorRegistry;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExampleOhosTest {
+    @Test
+    public void testBundleName() {
+        final String actualBundleName = AbilityDelegatorRegistry.getArguments().getTestBundleName();
+        assertEquals("com.starlong.miduapp.hmservice", actualBundleName);
+    }
+}
